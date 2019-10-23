ebc30a0cf0dab29ce2e9851a0dc4d2566a4145c1
Diego Brocanelli <diegod2@msn.com>
2019-10-23 17:43:25 -0300

    fix call scripts

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash 
 
 # Installing 
-source /scripts/hello.sh
+source scripts/hello.sh
 # Installing softwares
-source /scripts/softwares.sh
+source scripts/softwares.sh
 # Installing server
-source /scripts/server.sh
+source scripts/server.sh
 # Installing database
-source /scripts/databse.sh
+source scripts/databse.sh
 # Installing programming languages
-source /scripts/programming_languages.sh
+source scripts/programming_languages.sh
 # Installing tools
-source /scripts/tools.sh
+source scripts/tools.sh
 # Installing server configuration
-source /scripts/server_configuration.sh
+source scripts/server_configuration.sh
 # Installing vim configuration
-source /scripts/vim_configuration.sh
+source scripts/vim_configuration.sh
 # Installing config SO dracula theme
-source /scripts/config_so_dracula_theme.sh
+source scripts/config_so_dracula_theme.sh
 # Installing checking installed packages
-source /scripts/checking_installed_packages.sh
+source scripts/checking_installed_packages.sh
 # process completion
-source /scripts/finalization.sh
+source scripts/finalization.sh
