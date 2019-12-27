4c98d8345f3489e4821eb3932db9ea2f8c2777bb
Diego Brocanelli <diegod2@msn.com>
2019-12-27 10:24:10 -0300

    alter install sublime-text

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -31,7 +31,7 @@ fi
 if [ ${packages[sublime-text]} == true ]
 then
     echo "    Sublime Text...."
-    sudo snap install sublime-text --classic 1> /dev/null 2> /dev/stdout
+    sudo apt install sublime-text 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[code]} == true ]
