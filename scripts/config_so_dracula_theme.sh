e7d6a9b224bd08536c26ad5842bf84eb16820bf5
Diego Brocanelli <diegod2@msn.com>
2019-12-27 09:01:09 -0300

    Add sudo command.

--- scripts/config_so_dracula_theme.sh
+++ scripts/config_so_dracula_theme.sh
@@ -11,16 +11,16 @@ echo "    Cloning to Ant-Dracula...."
 
 sudo mkdir ~/.themes
 
-git clone https://github.com/Diego-Brocanelli/Ant-Dracula.git ~/.themes/Ant-Dracula 1> /dev/null 2> /dev/stdout
+sudo git clone https://github.com/Diego-Brocanelli/Ant-Dracula.git ~/.themes/Ant-Dracula 1> /dev/null 2> /dev/stdout
 
 echo "    Set to Ant-Dracula theme...."
 
-gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
-gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
+sudo gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
+sudo gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
 
 echo "    Set to Dracula wallpaper...."
 
-cp ./img/dracula_wallpaper.png $HOME/Imagens/ 1> /dev/null 2> /dev/stdout
-gsettings set org.gnome.desktop.background picture-uri file://$HOME/Downloads/dracula_wallpaper.png 1> /dev/null 2> /dev/stdout
+sudo cp ./img/dracula_wallpaper.png $HOME/Imagens/ 1> /dev/null 2> /dev/stdout
+sudo gsettings set org.gnome.desktop.background picture-uri file://$HOME/Imagens/dracula_wallpaper.png 1> /dev/null 2> /dev/stdout
 
 echo ""
