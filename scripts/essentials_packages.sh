9a81ab4356a5aee4071fc291e69c49dbad537c43
Diego Brocanelli <diegod2@msn.com>
2019-12-29 10:23:38 -0300

    command call before text not to break the aesthetics of data display in terminal

--- scripts/essentials_packages.sh
+++ scripts/essentials_packages.sh
@@ -4,10 +4,11 @@
 # Responsible for installing the essential packages
 #--------------------------------------------------
 
+sudo apt update -y 1> /dev/null 2> /dev/stdout
+
 title_echo "INSTALLING ESSENTIAL PACKAGES"
 
 line_echo "Updating packages"
-sudo apt update -y 1> /dev/null 2> /dev/stdout
 
 line_echo "build-essential package"
 sudo apt install build-essential -y 1> /dev/null 2> /dev/stdout
