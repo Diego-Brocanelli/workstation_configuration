46a606e997b5f6d51d2d85a0669b10064632f47e
Diego Brocanelli <diegod2@msn.com>
2019-12-29 10:35:48 -0300

    fix text

--- scripts/server_configuration.sh
+++ scripts/server_configuration.sh
@@ -7,37 +7,37 @@ sudo chown -R $USER:$USER /var/www 1> /dev/null 2> /dev/stdout
 
 if [ ${packages[nginx]} = true ]
 then
-    line_echo "Restart do nginx"
+    line_echo "Restart nginx"
     sudo service nginx restart 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[apache2]} = true ]
 then
-    line_echo "Restart do apache2"
+    line_echo "Restart apache2"
     sudo service apache2 restart 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[php5.6-fpm]} = true ]
 then
-    line_echo "Restart do PHP5.6"
+    line_echo "Restart PHP5.6"
     sudo service php5.6-fpm restart 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[php7.2-fpm]} = true ]
 then
-    line_echo "Restart do PHP7.2"
+    line_echo "Restart PHP7.2"
     sudo service php7.2-fpm restart 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[php7.3-fpm]} = true ]
 then
-    line_echo "Restart do PHP7.3"
+    line_echo "Restart PHP7.3"
     sudo service php7.3-fpm restart 1> /dev/null 2> /dev/stdout
 fi
 
 if [ ${packages[php7.4-fpm]} = true ]
 then
-    line_echo "Restart do PHP7.4"
+    line_echo "Restart PHP7.4"
     sudo service php7.4-fpm restart 1> /dev/null 2> /dev/stdout
 
     line_echo "Configurando PHP7.4 como default no cli"
