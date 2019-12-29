010352146d42942dfc64944dfc79c23f43a6c7ef
Diego Brocanelli <diegod2@msn.com>
2019-12-29 09:07:39 -0300

    fix command data output

--- scripts/essentials_packages.sh
+++ scripts/essentials_packages.sh
@@ -5,13 +5,13 @@
 #--------------------------------------------------
 
 echo "    Updating packages...."
-sudo apt update -y 1> /devull 2> /dev/stdout
+sudo apt update -y 1> /dev/null 2> /dev/stdout
 
 echo "    Installing build-essential package...."
-sudo apt install build-essential -y 1> /devull 2> /dev/stdout
+sudo apt install build-essential -y 1> /dev/null 2> /dev/stdout
 
 echo "    Installing deb-conf-utils package...."
-sudo apt install debconf-utils -y 1> /devull 2> /dev/stdout
+sudo apt install debconf-utils -y 1> /dev/null 2> /dev/stdout
 
 echo "    Installing binutils package...."
-sudo apt install binutils -y 1> /devull 2> /dev/stdout
+sudo apt install binutils -y 1> /dev/null 2> /dev/stdout
