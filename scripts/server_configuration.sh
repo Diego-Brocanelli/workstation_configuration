48e814b1c2c713cffb3f5a83e011d83a4b1f4ab8
Diego Brocanelli <diegod2@msn.com>
2020-12-20 11:41:18 -0300

    Implemented the dynamic text display structure as a result of the process performed, including PHP8.0 settings.

--- scripts/server_configuration.sh
+++ scripts/server_configuration.sh
@@ -2,46 +2,79 @@
 
 title_echo "CONFIGURATE TO SERVER "
 
-line_echo "Set user to /var/www directory...."
+install_echo "Set user to /var/www directory"
+
 sudo chown -R $USER:$USER /var/www 1> /dev/null 2> /dev/stdout
+
+success_install_echo "finalized user configuration for the directory /var/www"
 
 if [ ${packages[nginx]} = true ]
 then
-    line_echo "Restart nginx"
+    install_echo "Restart nginx"
+
     sudo service nginx restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "Nginx restart completed"
 fi
 
 if [ ${packages[apache2]} = true ]
 then
-    line_echo "Restart apache2"
+    install_echo "Restart apache2"
+
     sudo service apache2 restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "Apache2 restart completed"
 fi
 
 if [ ${packages[php5.6-fpm]} = true ]
 then
-    line_echo "Restart PHP5.6"
+    install_echo "Restart PHP5.6"
+
     sudo service php5.6-fpm restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "PHP5.6 restart completed"
 fi
 
 if [ ${packages[php7.2-fpm]} = true ]
 then
-    line_echo "Restart PHP7.2"
+    install_echo "Restart PHP7.2"
+
     sudo service php7.2-fpm restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "PHP7.2 restart completed"
 fi
 
 if [ ${packages[php7.3-fpm]} = true ]
 then
-    line_echo "Restart PHP7.3"
+    install_echo "Restart PHP7.3"
+
     sudo service php7.3-fpm restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "PHP7.3 restart completed"
 fi
 
 if [ ${packages[php7.4-fpm]} = true ]
 then
-    line_echo "Restart PHP7.4"
+    install_echo "Restart PHP7.4"
+
     sudo service php7.4-fpm restart 1> /dev/null 2> /dev/stdout
 
-    line_echo "Configurando PHP7.4 como default no cli"
-    sudo update-alternatives --set php /usr/bin/php7.4 1> /dev/null 2> /dev/stdout
+    success_install_echo "PHP7.4 restart completed"
+fi
+
+if [ ${packages[php8.0-fpm]} = true ]
+then
+    install_echo "Restart PHP8.0"
+
+    sudo service php7.4-fpm restart 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "PHP8.0 restart completed"
+
+    install_echo "Setting PHP8.0 as default in the cli"
+
+    sudo update-alternatives --set php /usr/bin/php8.0 1> /dev/null 2> /dev/stdout
+
+    install_echo "Finished PHP8.0 configuration as default in the cli"
 fi
 
 echo ""
