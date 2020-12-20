ce1d4eb977274e1ca6daa8fda95f7f349469c374
Diego Brocanelli <diegod2@msn.com>
2020-12-20 11:42:15 -0300

    Deprecated use of installation by snap, used alternatives to download .dep files or apt packages.

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -25,12 +25,6 @@ then
     git config --global core.editor vim
 fi
 
-if [ ${packages[snapd]} = true ]
-then
-    line_echo "SNAP"
-    sudo apt install snapd -y 1> /dev/null 2> /dev/stdout
-fi
-
 if [ ${packages[synapse]} = true ]
 then
     line_echo "Synapse"
@@ -41,20 +35,29 @@ fi
 
 if [ ${packages[aws-cli]} = true ]
 then
-    line_echo "AWS Cli"
-    sudo snap install aws-cli --classic 1> /dev/null 2> /dev/stdout
-fi
+    install_echo "AWS Cli V.2"
+    
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "./temp/awscliv2.zip" 1> /dev/null 2> /dev/stdout
+    unzip ./temp/awscliv2.zip 1> /dev/null 2> /dev/stdout
+    rm ./temp/awscliv2.zip 1> /dev/null 2> /dev/stdout
+    rm ./aws 1> /dev/null 2> /dev/stdout
 
-if [ ${packages[jenkins]} = true ]
-then
-    line_echo "Jenkins"
-    sudo snap install jenkins --classic 1> /dev/null 2> /dev/stdout
+    success_install_echo "AWS Cli V.2"
 fi
 
 if [ ${packages[docker]} = true ]
 then
-    line_echo "Docker"
-    sudo snap install docker 1> /dev/null 2> /dev/stdout
+    install_echo "Docker"
+
+    sudo apt update 1> /dev/null 2> /dev/stdout
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common 1> /dev/null 2> /dev/stdout
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 1> /dev/null 2> /dev/stdout
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" 1> /dev/null 2> /dev/stdout
+    sudo apt update 1> /dev/null 2> /dev/stdout
+    sudo apt install docker-ce 1> /dev/null 2> /dev/stdout
+    sudo systemctl status docker 1> /dev/null 2> /dev/stdout
+
+    success_install_echo "Docker"
 fi
 
 echo ""
