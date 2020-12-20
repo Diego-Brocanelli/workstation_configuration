bdb95abd2e14b32efef1f0899eb3f7e81ffa49c2
Diego Brocanelli <diegod2@msn.com>
2020-12-20 11:38:12 -0300

    Verification structure implemented to analyze if there is at least 1 software to be installed.

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -10,29 +10,39 @@ header_echo "### Author: Diego Brocanelli                                       
 header_echo "###                                                                        #"
 header_echo "############################################################################"
 echo ""
-line_danger_echo "ALERT!!!"
-line_danger_echo "THIS SCRIPT WILL UPDATE THE APT PACKAGES ON YOUR COMPUTER!"
+danger_echo "***********************ALERT******************************"
+danger_echo "THIS SCRIPT WILL UPDATE THE APT PACKAGES ON YOUR COMPUTER!"
+danger_echo "**********************************************************"
 echo ""
 
 title_echo "SOFTWARES TO BE INSTALLED"
 
+total_items_to_be_installed=0
 # Responsible to list the installed programs
 for package in "${!packages[@]}"
 do
     if [ ${packages[$package]} = true ] 
     then
+        total_items_to_be_installed=1
         line_echo "$package"
     fi
 done
 
+# at least 1 item to be installed must be included in the list for the process sequence.
+if [ $i = 0 ]
+then 
+    echo ""
+    danger_echo "No software selected! Please configure in the setp.sh file"
+    exit 0
+fi
 echo ""
 
-title_echo "Do you want continue? (1) Yes (0) No"
+question_echo "Do you want continue? (1) Yes (0) No"
 read continue
 
 if [ $continue = 0 ]
 then
-    title_echo "OK...process interrupted."
+    exclamation_echo "OK...process interrupted."
     exit
 fi
 
