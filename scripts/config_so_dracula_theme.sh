a98f90399cba6a5d66ccbd79cda6d04d01b17645
Diego Brocanelli <diegod2@msn.com>
2020-12-20 11:34:43 -0300

    Implemented structure to paint dynamic text according to the executed process, being success or failure.

--- scripts/config_so_dracula_theme.sh
+++ scripts/config_so_dracula_theme.sh
@@ -2,20 +2,26 @@
 
 title_echo "CONFIGURATE S.O. THEME"
 
-line_echo "Cloning to Ant-Dracula"
+install_echo "Cloning to Ant-Dracula"
 
 mkdir ~/.themes
 
 git clone https://github.com/Diego-Brocanelli/Ant-Dracula.git ~/.themes/Ant-Dracula 1> /dev/null 2> /dev/stdout
 
-line_echo "Set to Ant-Dracula theme"
+success_install_echo "Finished cloning to Ant-Dracula"
+
+install_echo "Set to Ant-Dracula theme"
 
 gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
 gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula" 1> /dev/null 2> /dev/stdout
+
+success_install_echo "Finished setting up the Dracula theme"
 
 line_echo "Set to Dracula wallpaper"
 
 cp ./img/dracula_wallpaper.png $HOME/Imagens/ 1> /dev/null 2> /dev/stdout
 gsettings set org.gnome.desktop.background picture-uri file://$HOME/Imagens/dracula_wallpaper.png 1> /dev/null 2> /dev/stdout
+
+success_install_echo "Finished setting the Dracula wallpaper"
 
 echo ""
