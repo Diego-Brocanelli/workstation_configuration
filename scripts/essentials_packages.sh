b9f001d23c75cb37376782231f00f22474904839
Diego Brocanelli <diegod2@msn.com>
2020-12-20 11:36:41 -0300

    Implementation of a structure for installing packages with dynamic text print as a result of the process, which may be successful or failed.

--- scripts/essentials_packages.sh
+++ scripts/essentials_packages.sh
@@ -8,15 +8,13 @@ sudo apt update -y 1> /dev/null 2> /dev/stdout
 
 title_echo "INSTALLING ESSENTIAL PACKAGES"
 
-line_echo "Updating packages"
+install_echo "Updating packages"
+success_install_echo "Finished updating packages"
 
-line_echo "build-essential package"
-sudo apt install build-essential -y 1> /dev/null 2> /dev/stdout
-
-line_echo "deb-conf-utils package"
-sudo apt install debconf-utils -y 1> /dev/null 2> /dev/stdout
-
-line_echo "binutils package"
-sudo apt install binutils -y 1> /dev/null 2> /dev/stdout
+process_install_echo "build-essential" "Build Essential"
+process_install_echo "debconf-utils" "Debconf Utils"
+process_install_echo "binutils" "Binutils"
+process_install_echo "wget" "Wget"
+process_install_echo "unzip" "Unzip"
 
 echo ""
