367f69052f60b1353c4d31d7d91ca1a718aae96d
Diego Brocanelli <diegod2@msn.com>
2021-01-02 10:08:43 -0300

    Changed Docker installation process.

--- scripts/softwares.sh
+++ scripts/softwares.sh
@@ -46,19 +46,19 @@ if [ ${packages[docker]} = true ]
 then
     install_echo "Starting installation: Docker"
 
-    sudo sudo apt-get install \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg-agent \
-        software-properties-common 1> /dev/null 2> /dev/stdout
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 1> /dev/null 2> /dev/stdout
-    sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable"
     sudo apt update -y 1> /dev/null 2> /dev/stdout
-    sudo apt install docker-ce docker-ce-cli containerd.io -y 1> /dev/null 2> /dev/stdout
+
+    sudo apt -y install apt-transport-https ca-certificates curl software-properties-common 1> /dev/null 2> /dev/stdout
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 1> /dev/null 2> /dev/stdout
+
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(. /etc/os-release; echo "$UBUNTU_CODENAME") stable" 1> /dev/null 2> /dev/stdout
+
+    sudo apt update -y 1> /dev/null 2> /dev/stdout
+
+    sudo apt-get -y install docker-ce 1> /dev/null 2> /dev/stdout
+
+    sudo usermod -aG docker $USER 1> /dev/null 2> /dev/stdout
 
     success_install_echo "Finished installation: Docker"
 fi
